f93e224983ac914b12dd80c374dc2d57147fb8d7
Zain541 <zainfarooq541@gmail.com>
2024-05-06 12:43:03 +0500

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { slot } from 'expo-router';
+
+const RootLayout = () => {
+  return (
+	<View style={styles.container}>
+	  <Text>RootLayout</Text>
+	</View>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
